23ed172f058b438f296073cee298d7d0144b94d6
mlodykeby <mlodykebytt@gmail.com>
2026-04-27 17:33:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     freeChannelId: "1498315468862128128",
     premiumChannelId: "1498315499849912486",
-    privateChannelId: "1498315534880735242"
+    privateChannelId: "1498315534880735242",
 
      dashboardChannelId: "1498343789092405338",
 
