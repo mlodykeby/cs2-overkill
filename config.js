9868bd8dad4dce27ac73273c9f9053f397f7be96
mlodykeby <mlodykebytt@gmail.com>
2026-04-27 17:26:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,10 @@ module.exports = {
     freeChannelId: "1498315468862128128",
     premiumChannelId: "1498315499849912486",
     privateChannelId: "1498315534880735242"
+
+     dashboardChannelId: "1498343789092405338",
+
+    affiliate: {
+        pirateswap: "keby"
+    }
 };
